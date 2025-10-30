1578535811c7ce5adb703abb11ae1abd2945fc6d
anika2711garg <1406anika@gmail.com>
2025-10-30 16:54:04 +0530

    Time: 3 ms (100%), Space: 56.8 MB (73.8%) - LeetHub

---
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int ans = target[0];
+        int n = target.length;
+        for( int i = 1; i < n; i++)
+        {
+            if( target[i] > target[i-1])
+            {
+                ans = ans + target[i] - target[i-1];
+            }
+        }
+        return ans;
+    }
+}
