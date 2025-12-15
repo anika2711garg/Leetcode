d79d645c4e9b18551f465681abfb4bad8b8a9094
anika2711garg <1406anika@gmail.com>
2025-12-15 22:01:36 +0530

    Time: 20 ms (92.8%), Space: 47.6 MB (59.2%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
@@ -2,35 +2,24 @@ class Solution {
     public int numberOfWays(String corridor) {
         long mod = 1000000007;
         long ways = 1;
-        int seatCount = 0;
+
+        int seats = 0;     
+        int last = -1;     
         int n = corridor.length();
 
-        int lastSeatIndex = -1;      // stores the index of previous seat
-        boolean firstPairDone = false;
-
         for (int i = 0; i < n; i++) {
-            char c = corridor.charAt(i);
+            if (corridor.charAt(i) == 'S') {
+                seats++;
 
-            if (c == 'S') {
-                seatCount++;
-
-                // If this is a new pair starting
-                if (seatCount % 2 == 1) {
-                    // odd number seat -> start of new pair
-                    if (lastSeatIndex != -1 && firstPairDone) {
-                        // all previous pairs are complete
-                        ways = (ways * (i - lastSeatIndex)) % mod;
-                    }
-                } else {
-                    // even seat -> pair completed
-                    lastSeatIndex = i;
-                    firstPairDone = true;
+                if (seats >= 3 && seats % 2 == 1) {
+                    ways = (ways * (i - last)) % mod;
                 }
+
+                last = i;
             }
         }
 
-        // total seats must be even and >= 2
-        if (seatCount < 2 || seatCount % 2 == 1) return 0;
+        if (seats == 0 || seats % 2 == 1) return 0;
 
         return (int) ways;
     }
