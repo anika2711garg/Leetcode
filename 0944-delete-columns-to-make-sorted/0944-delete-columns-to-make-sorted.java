11530229863e77c9fe7e620b6215e25f2fb8d3e1
anika2711garg <1406anika@gmail.com>
2025-12-23 21:00:59 +0530

    Time: 7 ms (73.94%), Space: 47 MB (72.05%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -1,17 +1,17 @@
 class Solution {
     public int minDeletionSize(String[] s) {
-        int n = s.length, m = s[0].length();
+        int n = s.length;
+        int m = s[0].length();
         int count = 0;
 
-        for (int row_concrete = 0; row_concrete < m; row_concrete++) {
-            for (int row = 1; row < n; row++) {
-                if (s[row].charAt(row_concrete) < s[row - 1].charAt(row_concrete)) {
-                    count++;
-                    break;
+        for (int col = 0; col < m; col++) {
+            for (int row = 0; row < n - 1; row++) {
+                if (s[row].charAt(col) > s[row + 1].charAt(col)) {
+                    count++;   
+                    break;     
                 }
             }
         }
-
         return count;
     }
 }
