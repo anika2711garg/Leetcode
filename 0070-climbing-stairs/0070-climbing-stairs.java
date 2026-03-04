ea76687d11b0be9a3620ce44264211de5fe6083a
anika2711garg <1406anika@gmail.com>
2026-03-04 19:49:38 +0530

    Time: 0 ms (100%), Space: 42 MB (73.51%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int climbStairs(int n) {
+       if( n <= 2)
+       {
+        return n;
+       }
+       else{
+             int f1 = 1;
+             int f2 = 2;
+             int result = 0;
+             for( int i = 3; i <= n; i++)
+             {
+                result = f1 + f2;
+                f1 = f2;
+                f2 = result;
+             }
+             return result;
+       }
+       
+        
+    }
+}
