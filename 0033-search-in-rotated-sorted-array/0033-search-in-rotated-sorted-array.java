03bfe1a61cf657298c051dc1007b8016b77cd480
anika2711garg <1406anika@gmail.com>
2026-04-11 10:34:12 +0530

    Time: 0 ms (100%), Space: 43.6 MB (93.01%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,56 +1,39 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n = nums.length;
-        int low = 0; 
+        int low = 0;
         int high = n-1;
         while(low <= high)
         {
-            int mid = (low+high)/2;
+            int mid = (high+low)/2;
             if(nums[mid] == target)
             {
                 return mid;
             }
             if(nums[low] <= nums[mid])
             {
-                if(nums[low] <= target && target < nums[mid])
+                if(nums[low] <= target && target <= nums[mid])
                 {
-                    high = mid - 1;
+                    high = mid-1;
                 }
                 else
-                {
-                    low = mid + 1;
-                }
-            }
-            else
-            {
-                if(nums[mid] < target && target <= nums[high])
                 {
                     low = mid+1;
                 }
-                else
-                {
-                    high = mid - 1;
-                }
+            }
+          else
+          {
+            if(nums[mid] <= target && target <= nums[high])
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
             }
         }
+        }
 
-        return - 1;
+      return -1;  
     }
 }
-
-// class Solution {
-//     public int search(int[] nums, int target) {
-//         int n = nums.length;
-//             for( int i = 0; i < n; i++)
-//             {
-//                 if(nums[i] == target)
-//             {
-                
-//             return i;
-//             }
-
-//         }
-//         return -1;
-        
-//     }
-// }
