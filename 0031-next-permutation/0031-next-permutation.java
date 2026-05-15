97e134a8f13e6bf930627ddc977b6b2af0174678
anika2711garg <1406anika@gmail.com>
2026-05-15 17:51:30 +0530

    Time: 0 ms (100%), Space: 44.7 MB (32.02%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -2,10 +2,11 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
         int i = n-2;
-        while( i >= 0 && nums[i] >= nums[i+1])
+
+        while(i >= 0 && nums[i] >= nums[i+1])
         {
             i--;
-        }
+        }   
         if( i >= 0)
         {
             int j = n-1;
@@ -14,24 +15,24 @@ class Solution {
                 j--;
             }
             swap(nums,i,j);
-        }
-        reverse(nums,i+1);
-        
+        } 
+           reverse(nums,i+1);
     }
-    private void swap(int[] nums, int i, int j)
-    {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    private void reverse(int[] nums, int st)
-    {
-        int end = nums.length - 1;
-        while(st < end)
+
+        private void swap (int[] nums, int i, int j)
         {
-            swap(nums,st,end);
-            st++;
-            end--;
-        }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        } 
+        private void reverse(int[] nums, int st)
+        {
+            int end = nums.length-1;
+            while(st < end)
+            {
+                swap(nums,st,end);
+                st++;
+                end--;
+            }
+        }   
     }
-}
