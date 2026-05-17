1ece6e5f7827362fd0dac1b5a4ac977a0ab98d6c
anika2711garg <1406anika@gmail.com>
2026-05-17 12:37:24 +0530

    Time: 0 ms (100%), Space: 43.8 MB (32.4%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,40 +1,38 @@
 class Solution {
     public void rotate(int[][] matrix) {
         Transpose_matrix(matrix);
-        Reverse_Rows(matrix);
+        Reverse_rows(matrix);
     }
-
-        private void Transpose_matrix(int matrix[][])
+    private void Transpose_matrix(int[][] matrix)
+    {
+        int n = matrix.length;
+        for( int i = 0; i < n; i++)
         {
-            int n = matrix.length;
-            for( int i = 0; i < n; i++)
+            for( int j = i ; j < n; j++)
             {
-                for( int j = i; j < n; j++)
-                {
-                    int temp = matrix[i][j];
-                    matrix[i][j] = matrix[j][i];
-                    matrix[j][i] =  temp;
-                }
-            }
-        }
-
-            private void Reverse_Rows( int matrix[][])
-            {
-                int n = matrix.length;
-                for( int i = 0; i < n; i++)
-                {
-                int left = 0;
-                int right = n-1;
-
-                while( left < right)
-                {
-                    int temp = matrix[i][left];
-                    matrix[i][left] = matrix[i][right];
-                    matrix[i][right] = temp;
-                    left++;
-                    right--;
-                
-                }
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
     }
+
+    private void Reverse_rows(int[][] matrix)
+    {
+        int n = matrix.length;
+        for(int i = 0; i < n; i++)
+        {
+            int left = 0;
+            int right = n-1;
+
+            while(left < right)
+            {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
