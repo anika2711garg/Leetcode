c419ce3c8d911f940c2e07649c2db99c880ce5ed
anika2711garg <1406anika@gmail.com>
2026-05-17 11:25:29 +0530

    Time: 0 ms (100%), Space: 43.7 MB (23.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -4,35 +4,33 @@ class Solution {
         int cnt0 = 0;
         int cnt1 = 0;
         int cnt2 = 0;
-        for( int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
-            if( nums[i] == 0)
-            {
-                cnt0++;
-            }
-            else if(nums[i] == 1)
-            {
-                 cnt1++;
-            }
-            else
-            {
-                cnt2++;
-            }
+           if(nums[i] == 0)
+           {
+            cnt0++;
+           } 
+           else if(nums[i] == 1)
+           {
+            cnt1++;
+           } 
+           else
+           {
+            cnt2++;
+           } 
         }
-       int idx = 0;
-        while( cnt0-- > 0)
-        {
-           nums[idx++] = 0;
-        }
-        while( cnt1-- > 0)
-        {
-           nums[idx++] = 1;
-        }
-        while( cnt2-- > 0)
-        {
-           nums[idx++] = 2;
-        }
-
+        int idx = 0;
+       while(cnt0-- > 0)
+       {
+        nums[idx++] = 0;
+       }while(cnt1-- > 0)
+       {
+        nums[idx++] = 1;
+       }
+        while(cnt2-- > 0)
+       {
+        nums[idx++] = 2;
+       }
         
     }
 }
