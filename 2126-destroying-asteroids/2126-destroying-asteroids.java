eaf46b28377ab6f820aba5d5cce170d407b982d9
anika2711garg <1406anika@gmail.com>
2026-05-31 08:59:27 +0530

    Time: 26 ms (81.52%), Space: 107.3 MB (65.13%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        int n = asteroids.length;
+        long currmass = mass;
+        for( int i = 0; i < n; i++)
+        {
+            if( currmass >= asteroids[i])
+            {
+                currmass += asteroids[i];
+            }
+            else
+            {
+                return false;
+            }
+        }
+       
+         return true;
+    }
+}
