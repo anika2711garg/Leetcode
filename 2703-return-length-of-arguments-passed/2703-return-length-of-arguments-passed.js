aa58c131caa7a5c8a1dbf4d5aab72562ba5710ee
anika2711garg <1406anika@gmail.com>
2026-06-15 23:18:37 +0530

    Time: 47 ms (32.74%), Space: 53.3 MB (68.96%) - LeetHub

---
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -0,0 +1,9 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    let count = 0;
+    while (args[count++] !== undefined) {}
+    return count-1;
+};
