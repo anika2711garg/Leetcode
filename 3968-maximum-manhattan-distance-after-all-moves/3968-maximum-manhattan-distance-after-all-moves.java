12b12e45b7326e82839a70a09f31e662826aa025
anika2711garg <1406anika@gmail.com>
2026-06-21 11:45:02 +0530

    Time: 16 ms (91.42%), Space: 48.1 MB (12.66%) - LeetHub

---
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDistance(String moves) {
+        int u = 0, d = 0, l = 0, r = 0, k = 0;
+
+        for (char ch : moves.toCharArray())
+        {
+            if (ch == 'U') u++;
+            else if (ch == 'D') d++;
+            else if (ch == 'L') l++;
+            else if (ch == 'R') r++;
+            else k++;
+        }
+
+        return Math.abs(r - l) + Math.abs(u - d) + k;
+    }
+}
