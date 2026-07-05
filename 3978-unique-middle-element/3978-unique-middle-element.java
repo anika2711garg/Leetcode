e4f1ec6f9e4e30db8d1576e30503c6cee21dd954
anika2711garg <1406anika@gmail.com>
2026-07-05 08:32:30 +0530

    Time: 1 ms (100%), Space: 46.6 MB (100%) - LeetHub

---
+++ 3978-unique-middle-element/3978-unique-middle-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isMiddleElementUnique(int[] nums) {
+        int n = nums.length;
+        int mid = nums[n / 2];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == mid) {
+                count++;
+            }
+        }
+
+        if (count == 1) {
+            return true;
+        }
+
+        return false;
+    }
+}
